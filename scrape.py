6fea0e2158b6045bd21f8121d453caf78ce6dcf0
Arsen <102597298+molchoun@users.noreply.github.com>
2022-07-22 01:37:26 +0400

    csv file changes

--- scrape.py
+++ scrape.py
@@ -27,12 +27,3 @@ def get_category_links(url: str, n: int) -> List[str]:
 
     return links
 
-
-dict_apt = {'Apartments': get_category_links('https://www.list.am/category/60/', 250)}
-df1 = pd.DataFrame(dict_apt)
-df1.to_csv('apartments-evn.csv')
-
-dict_houses = {'Houses': get_category_links('https://www.list.am/category/62/', 88)}
-df2 = pd.DataFrame(dict_houses)
-df2.to_csv('houses-evn.csv')
-
