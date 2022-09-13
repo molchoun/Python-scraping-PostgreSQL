f662d78d8c2c9e8e04c92a82d7b67f3c104eb9ba
molchoun <ne.molchoun@gmail.com>
2022-09-13 20:15:47 +0400

    add functions: page_soup() - to avoid copy/paste, get_category_url(), modified get_category_links() function to more optimal crawling performance

--- scrape.py
+++ scrape.py
@@ -1,3 +1,4 @@
+import re
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as soup
 import time
@@ -5,20 +6,64 @@ import pandas as pd
 from typing import List
 
 
-def get_category_links(url: str, n: int) -> List[str]:
+URL_HOME = 'https://www.list.am/en'
 
-    # Get all available pages urls in particular category
-    # '?n=1' - filter query to select apartments in Yerevan
-    cat_urls = [url + str(i) + '?n=1' for i in range(1, n)]
 
+def page_soup(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read().decode('utf-8')
+    pg_soup = soup(webpage, "html.parser")
+    return pg_soup
+
+
+def get_category_url():
+    url = 'https://www.list.am/en/category/54'
+    pg_soup = page_soup(url)
+    section_cat = pg_soup.select('div.s')
+    categories_dict = dict()
+    # Fill dictionary with categories names and links to respective pages:
+    # e.g 'Apartments for sale': '/category/60'
+    for cat in section_cat[2].select('a'):
+        categories_dict[cat.text + ' for sale'] = cat['href']
+    for cat in section_cat[3].select('a'):
+        categories_dict[cat.text + ' for rent'] = cat['href']
+
+    return categories_dict
+
+
+def get_location():
+    """
+    Returns dictionary: key being location name and value - query value
+    """
+    url = 'https://www.list.am/en/category/'
+    pg_soup = page_soup(url)
+
+    # select all `divs` that contain region names
+    data_searchname = pg_soup.find_all('div', {'class': 'i', 'data-name': re.compile('^[A-z]')}) # data-name helps to
+    # select only regions, without cities
+    loc_dict = dict()
+    for data in data_searchname:
+        if data['data-name'] not in loc_dict:
+            loc_dict[data['data-name']] = '?n=' + data['data-value']
+    return loc_dict
+
+
+def get_ad_links(url: str) -> List[str]:
     links = []
     a_tags = []
-    for url in cat_urls:
-        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        webpage = urlopen(req).read().decode('utf-8')
-        page_soup = soup(webpage, "html.parser")
-        a_tags += list(page_soup.select('div.gl a'))
-        # time.sleep(2)
+
+    while True: # code to handle pagination
+        pg_soup = page_soup(url)
+        # append list with `a` tags containing link to ad, e.g /en/item/16954298
+        a_tags.extend(pg_soup.select('div.gl a'))
+        # locate the `Next` button
+        next_page_element = pg_soup.find('a', text='Next >')
+        if next_page_element:
+            next_page_url = next_page_element.get('href')
+            url = 'https://www.list.am/en/' + next_page_url
+            print('')
+        else:
+            break
 
     for a in a_tags:
         full_url = 'https://list.am/en' + a['href']
@@ -26,4 +71,6 @@ def get_category_links(url: str, n: int) -> List[str]:
             links.append(full_url)
 
     return links
+
+
 
