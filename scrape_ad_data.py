2e3ff50e2afda0094d927ca0d96e22a52c0fdbb5
molchoun <movsisian.arsen@gmail.com>
2022-11-24 21:30:14 +0400

    add time sleep in the loop, add datetime to DataFrame

--- scrape_ad_data.py
+++ scrape_ad_data.py
@@ -4,7 +4,10 @@ from urllib.error import HTTPError
 from urllib.request import Request, urlopen
 import pandas as pd
 from bs4 import BeautifulSoup as soup
+from datetime import datetime
 import os
+import time
+import random
 
 
 def get_filenames(data_dir='data'):
@@ -33,22 +36,24 @@ def scrape_apt_ad_page():
             urls_data = df_ad_data['Links']
         except:
             df_ad_data = pd.DataFrame()
-            urls_data = pd.Series()
+            urls_data = pd.Series(dtype=float)
         urls = df[path_list[1]]
         counter = len(urls)
-
+        urls_temp = []
         for url_idx in range(len(urls)):
             if urls[url_idx] in urls_data.tolist():
+                continue
+            elif urls[url_idx].endswith('price') or urls[url_idx].endswith('removed'):
                 continue
             try:
                 req = Request(urls[url_idx], headers={'User-Agent': 'Mozilla/5.0'})
                 webpage = urlopen(req).read().decode('utf-8')
                 page_soup = soup(webpage, "html.parser")
                 if page_soup.find('span', class_='price') is None:  # delete url and skip if ad has no price provided
-                    urls.drop(index=url_idx, inplace=True)
+                    urls[url_idx] += ' no price'
                     continue
             except urllib.error.HTTPError or ValueError:
-                urls.drop(index=url_idx, inplace=True)
+                urls[url_idx] += ' ad removed'
                 continue
 
             div_title = page_soup.find_all('div', {'class': 't'})  # titles of apartment descriptive information: e.g.
@@ -63,6 +68,7 @@ def scrape_apt_ad_page():
                 address = page_soup.find('div', class_="loc").text
             price = page_soup.find('span', class_='price').text
             description = page_soup.find('div', class_='body').text
+            datetime_now = datetime.now().strftime("%b-%d-%Y_%H-%M")
 
             data_dict = {div_title[i].text: div_value[i].text for i in range(len(div_title))
                          if div_title[i].text != 'Places Nearby'}
@@ -70,15 +76,19 @@ def scrape_apt_ad_page():
             data_dict['Price'] = price
             data_dict['Currency'] = currency
             data_dict['Description'] = description
+            data_dict['Datetime'] = datetime_now
 
+            urls_temp.append(urls[url_idx])
             df_new_columns = pd.DataFrame([data_dict])
             df_ad_data = pd.concat([df_ad_data, df_new_columns], axis=0, ignore_index=True)
 
             counter -= 1
             print(f'{(1 - counter / len(urls)) * 100}% is done')
-
-            df_ad_data['Links'] = urls
-            df_ad_data.to_csv(f'{file}_data.csv', index=False)
+        df.loc[:, path_list[1]] = urls
+        df.to_csv(file, index=False)
+        df_ad_data.loc[len(df_ad_data)-len(urls_temp):, 'Links'] = urls_temp
+        df_ad_data.to_csv(f'{file}_data.csv', index=False)
+        time.sleep(random.randint(2, 4))
     print(f'done!')
     # return data_list, list(data_dict.keys())
 
